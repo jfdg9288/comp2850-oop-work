426e13ae47a247d1ae958627a6b6d79d764b06e4
jfdg9288 <jfdg9288@leeds.ac.uk>
2025-09-30 11:40:36 +0000

    a

---
+++ tasks/task2_5_1/kProgram.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var a = 5
+    println(a)
+    a = 6
+    println(a)
+}
---
+++ tasks/task2_5_2/kProg.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val myAge = 29u //Unsigned integer
+    val universeAge = 13_800_000_000L //long integer
+    val status = 'M' //char
+    val name = "Sarah" //string
+    val height = 1.78f //float
+    val root2 = Math.sqrt(2.0) //double
+
+    println(myAge::class)
+    println(universeAge::class)
+    println(status::class)
+    println(name::class)
+    println(height::class)
+    println(root2::class)
+}
